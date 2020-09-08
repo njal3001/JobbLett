4818208e92fb60f3a7fbd76ca0f3b18898d22e42
Hary Pirajan Mahalingam <harypm@stud.ntnu.no>
2020-09-08 13:52:52 +0200

    WIP: Added JavaFX Project files and updated pom.xml. Still not working

--- bolett/src/main/java/bolett/App.java
+++ bolett/src/main/java/bolett/App.java
@@ -1,13 +1,21 @@
 package bolett;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+  @Override
+  public void start(final Stage primaryStage) throws Exception {
+    final Parent parent = FXMLLoader.load(getClass().getResource("App.fxml"));
+    primaryStage.setScene(new Scene(parent));
+    primaryStage.show();
+  }
+
+  public static void main(final String[] args) {
+    launch(args);
+  }
 }
