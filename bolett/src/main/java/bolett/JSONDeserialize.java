277ce390ae9514e098c255b4b661a6465b74f400
Hary Pirajan Mahalingam <harypm@stud.ntnu.no>
2020-09-14 17:22:38 +0200

    WIP: Started on implementing Jackson Deserialization. (#13)

---
+++ bolett/src/main/java/bolett/JSONDeserialize.java
@@ -0,0 +1,40 @@
+package bolett;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+
+public class JSONDeserialize {
+    Main main;
+
+    public Main importJSON() {
+        Main main = null;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("src/main/java/bolett/main.json"));
+            String json = reader.readLine();
+
+            // create object mapper instance
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            // deserialize json string
+            main = objectMapper.readValue(json,Main.class);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.out.println(ex);
+        }
+
+        return main;
+    }
+
+    public static void main(String[] args) {
+        JSONDeserialize importer = new JSONDeserialize();
+        Main main = importer.importJSON();
+        System.out.println(main);
+
+    }
+}
