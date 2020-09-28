d801583c377df456c79a5ff0912a1d62e47ed110
Njål Brynhildarson Telstø <njaalbt@stud.ntnu.no>
2020-09-28 13:58:26 +0000

    Made JobShift comparable and added more functionality (#22)

--- jobblett/src/main/java/jobblett/core/JobShift.java
+++ jobblett/src/main/java/jobblett/core/JobShift.java
@@ -3,7 +3,7 @@ package jobblett.core;
 import java.time.Duration;
 import java.time.LocalDateTime;
 
-public class JobShift {
+public class JobShift implements Comparable<JobShift>{
 
     private User user;
     private LocalDateTime startingTime;
@@ -11,10 +11,10 @@ public class JobShift {
     private String info;
 
     public JobShift(User user, LocalDateTime startingTime, Duration duration, String info){
-        this.user = user;
-        this.startingTime = startingTime;
-        this.duration = duration;
-        this.info = info;
+        setUser(user);
+        setStartingTime(startingTime);
+        setDuration(duration);
+        setInfo(info);
     }
 
     public User getUser(){
@@ -29,9 +29,42 @@ public class JobShift {
         return duration;
     }
 
+    public LocalDateTime getEndingTime(){
+        return startingTime.plus(duration);
+    }
+
     public String getInfo(){
         return info;
     }
 
+    public void setUser(User user){
+        this.user = user;
+    }
 
+    public void setStartingTime(LocalDateTime startingTime){
+        this.startingTime = startingTime;
+    }
+
+    public void setDuration(Duration duration){
+        this.duration = duration;
+    }
+
+    public void setInfo(String info){
+        this.info = info;
+    }
+    
+    /**
+     * {@inheritDoc}
+     * @return a negative integer, zero, or a positive integer as the starting time of this job shift
+     * is less than, equal to, or greater than the starting time of the other job shift.
+     */
+    @Override
+    public int compareTo(JobShift other) {
+        if(this.startingTime.isAfter(other.startingTime))
+            return 1;
+        else if(this.startingTime.isBefore(other.startingTime))
+            return -1;
+        else
+            return 0;
+    }
 }
