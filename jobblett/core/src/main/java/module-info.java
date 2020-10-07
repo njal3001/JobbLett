3a5dfbf927d410d87fa05fbf9c24db4d4d73fcbe
Hary Pirajan Mahalingam <hary2309@gmail.com>
2020-10-07 20:58:38 +0200

    WIP: Modularizing project. (#24)

--- jobblett/core/src/main/java/module-info.java
+++ jobblett/core/src/main/java/module-info.java
@@ -1,7 +1,4 @@
-module jobblett {
-	requires javafx.fxml;
-	requires transitive javafx.graphics;
-	requires javafx.controls;
+module jobblett.core {
     requires com.fasterxml.jackson.databind;
     requires com.fasterxml.jackson.datatype.jdk8;
     requires com.fasterxml.jackson.datatype.jsr310;
@@ -9,9 +6,7 @@ module jobblett {
 
 	exports jobblett.core;
 	exports jobblett.json;
-	exports jobblett.ui;
 
-	opens jobblett.ui to javafx.fxml;
 	opens jobblett.json to com.fasterxml.jackson.databind;
 	opens jobblett.core to com.fasterxml.jackson.databind;
 }
