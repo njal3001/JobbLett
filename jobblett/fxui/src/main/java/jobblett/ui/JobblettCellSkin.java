530c66519e6ef543fafe73c8a52d9e80bafb7cff
Hary Pirajan Mahalingam <harypm@stud.ntnu.no>
2020-11-01 21:43:45 +0000

    Updated CellSkin to visualize selected cells. (#42)

--- jobblett/fxui/src/main/java/jobblett/ui/JobblettCellSkin.java
+++ jobblett/fxui/src/main/java/jobblett/ui/JobblettCellSkin.java
@@ -1,7 +1,10 @@
 package jobblett.ui;
 
+import javafx.beans.value.ChangeListener;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.skin.ListCellSkin;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
@@ -11,5 +14,12 @@ public class JobblettCellSkin<T> extends ListCellSkin<T> {
         super(listCell);
         listCell.setFont(Font.loadFont(JobblettCellSkin.class.getResourceAsStream(App.FONT_FILE),16));
         listCell.setTextFill(Color.BLACK);
+        listCell.selectedProperty().addListener((observable, oldValue, newValue) -> {
+          if (listCell.isSelected()) {
+            listCell.setStyle("-fx-background-color: #B8D8D8");
+          } else {
+            listCell.setStyle("-fx-background-color: #FFFFFF");
+          }
+        });
     }
 }
