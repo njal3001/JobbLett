f0d6aca75056bdcf5fbac8040acf9f5d9f9fd128
Hary Pirajan Mahalingam <hary2309@gmail.com>
2020-11-04 15:34:40 +0100

    Fixed checkstyle errors on Restapi-module and Restserver-module. (#53)

--- jobblett/restapi/src/main/java/jobblett/restapi/JobblettService.java
+++ jobblett/restapi/src/main/java/jobblett/restapi/JobblettService.java
@@ -1,61 +1,66 @@
 package jobblett.restapi;
 
-import jobblett.core.GroupList;
-import jobblett.core.JobblettList;
-import jobblett.core.User;
-import jobblett.core.UserList;
-import jobblett.json.JobblettDeserializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import javax.inject.Inject;
-import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
+import static jobblett.restapi.GroupListResource.GROUP_LIST_SERVICE_PATH;
+import static jobblett.restapi.UserListResource.USER_LIST_SERVICE_PATH;
 
 import java.util.Collection;
 import java.util.Iterator;
-
-import static jobblett.restapi.GroupListResource.GROUP_LIST_SERVICE_PATH;
-import static jobblett.restapi.UserListResource.USER_LIST_SERVICE_PATH;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import jobblett.core.GroupList;
+import jobblett.core.JobblettList;
+import jobblett.core.UserList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 @Path(JobblettService.JOBBLETT_SERVICE_PATH)
 public class JobblettService {
-    public static final String JOBBLETT_SERVICE_PATH = "jobblett";
-    protected static final Logger LOG = LoggerFactory.getLogger(JobblettService.class);
+  public static final String JOBBLETT_SERVICE_PATH = "jobblett";
+  protected static final Logger LOG = LoggerFactory.getLogger(JobblettService.class);
 
-    @Inject
-    private GroupList groupList;
+  @Inject private GroupList groupList;
 
-    @Inject
-    private UserList userList;
+  @Inject private UserList userList;
 
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public GroupList getGroupList() {
-        return groupList;
-    }
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  public GroupList getGroupList() {
+    return groupList;
+  }
 
-    @Path("/"+ GROUP_LIST_SERVICE_PATH)
-    public GroupListResource getGroupListResource() {
-        return new GroupListResource(groupList);
-    }
+  @Path("/" + GROUP_LIST_SERVICE_PATH)
+  public GroupListResource getGroupListResource() {
+    return new GroupListResource(groupList);
+  }
 
-    @Path("/"+ USER_LIST_SERVICE_PATH)
-    public UserListResource getUserListResource() {
-        return new UserListResource(userList);
-    }
+  @Path("/" + USER_LIST_SERVICE_PATH)
+  public UserListResource getUserListResource() {
+    return new UserListResource(userList);
+  }
 
-    @POST
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("/setlists/{userListAndGroupListStrings}")
-    public boolean login(@PathParam("userListAndGroupListStrings") Collection<JobblettList> userListAndGroupListStrings){
-        Iterator<JobblettList> iterator = userListAndGroupListStrings.iterator();
-        UserList userList = (UserList) iterator.next();
-        GroupList groupList = (GroupList) iterator.next();
-        this.userList = userList;
-        this.groupList = groupList;
-        return true;
-    }
+  /**
+   * TODO.
+   *
+   * @param userListAndGroupListStrings TODO
+   * @return TODO
+   */
+  @POST
+  @Produces(MediaType.APPLICATION_JSON)
+  @Path("/setlists/{userListAndGroupListStrings}")
+  public boolean login(@PathParam("userListAndGroupListStrings")
+      Collection<JobblettList> userListAndGroupListStrings) {
+    Iterator<JobblettList> iterator = userListAndGroupListStrings.iterator();
+    UserList userList = (UserList) iterator.next();
+    GroupList groupList = (GroupList) iterator.next();
+    this.userList = userList;
+    this.groupList = groupList;
+    return true;
+  }
 
 }
--- jobblett/restapi/src/main/java/module-info.java
+++ jobblett/restapi/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module jobblett.restapi {
-    requires javax.inject;
-    requires java.ws.rs;
-    requires slf4j.api;
-    requires jobblett.core;
-    requires jobblett.json;
+  requires javax.inject;
+  requires java.ws.rs;
+  requires slf4j.api;
+  requires jobblett.core;
+  requires jobblett.json;
 }
