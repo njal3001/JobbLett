ef1fec0e4f4b16fc5f9cc5ef44d03e7c2bd9d7ed
sanketb <sanketb@stud.ntnu.no>
2020-11-20 03:00:39 +0100

    WIP: restAPI, added more tests

--- jobblett/restserver/src/test/java/jobblett/restserver/JobblettServiceTest.java
+++ jobblett/restserver/src/test/java/jobblett/restserver/JobblettServiceTest.java
@@ -16,6 +16,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.Iterator;
@@ -24,18 +25,17 @@ import static jobblett.restapi.JobShiftListResource.JOB_SHIFT_LIST_RESOURCE_PATH
 import static org.junit.jupiter.api.Assertions.*;
 
 
-public class JobblettServiceTest extends JerseyTest{
+public class JobblettServiceTest extends JerseyTest {
 
-  protected boolean shouldLog(){
+  protected boolean shouldLog() {
     return false;
   }
 
 
-  @Override
-  protected ResourceConfig configure(){
+  @Override protected ResourceConfig configure() {
     Workspace workspace = new JobblettPersistence().readDefault(Workspace.class);
     final JobblettConfig jobblettConfig = new JobblettConfig(workspace);
-    if(shouldLog()){
+    if (shouldLog()) {
       enable(TestProperties.LOG_TRAFFIC);
       enable(TestProperties.DUMP_ENTITY);
       jobblettConfig.property(LoggingFeature.LOGGING_FEATURE_LOGGER_LEVEL_SERVER, "WARNING");
@@ -43,40 +43,33 @@ public class JobblettServiceTest extends JerseyTest{
     return jobblettConfig;
   }
 
-  @Override
-  protected TestContainerFactory getTestContainerFactory() throws TestContainerException {
+  @Override protected TestContainerFactory getTestContainerFactory() throws TestContainerException {
     return new GrizzlyTestContainerFactory();
   }
 
   private ObjectMapper objectMapper;
 
-  @Override
-  @BeforeEach
-  public void setUp() throws Exception{
+  @Override @BeforeEach public void setUp() throws Exception {
     super.setUp();
     objectMapper = new JobblettModuleObjectMapperProvider().getContext(getClass());
     System.out.println(objectMapper);
   }
 
-  @Override
-  @AfterEach
-  public void tearDown() throws Exception{
+  @Override @AfterEach public void tearDown() throws Exception {
     super.tearDown();
   }
 
-  private void assertUser(User user, String username, String givenName, String familyName){
+  private void assertUser(User user, String username, String givenName, String familyName) {
     assertEquals(user.getUsername(), username);
     assertEquals(user.getGivenName(), givenName);
     assertEquals(user.getFamilyName(), familyName);
   }
 
-  @Test
-  public void getUserListTest(){
+  @Test public void getUserListTest() {
     Response getResponse = target(WorkspaceService.WORKSPACE_SERVICE_PATH).path("userlist")
-        .request(MediaType.APPLICATION_JSON + ";" + MediaType.CHARSET_PARAMETER+"=UTF8")
-        .get();
+        .request(MediaType.APPLICATION_JSON + ";" + MediaType.CHARSET_PARAMETER + "=UTF8").get();
     assertEquals(200, getResponse.getStatus());
-    try{
+    try {
       UserList userList = objectMapper.readValue(getResponse.readEntity(String.class), UserList.class);
       Iterator<User> iterator = userList.iterator();
       assertTrue(iterator.hasNext());
@@ -93,104 +86,76 @@ public class JobblettServiceTest extends JerseyTest{
       assertUser(user3, "petter", "Petter", "Petterson");
       assertUser(user4, "david", "David", "Berg");
 
-    } catch (JsonProcessingException e){
+    } catch (JsonProcessingException e) {
       fail(e.getMessage());
     }
   }
 
-  @Test
-  public void getGroupListTest(){
+  @Test public void getGroupListTest() {
     Response getResponse = target(WorkspaceService.WORKSPACE_SERVICE_PATH).path("grouplist")
-        .request(MediaType.APPLICATION_JSON + ";" + MediaType.CHARSET_PARAMETER+"=UTF8")
-        .get();
+        .request(MediaType.APPLICATION_JSON + ";" + MediaType.CHARSET_PARAMETER + "=UTF8").get();
     assertEquals(200, getResponse.getStatus());
-    try{
-      GroupList groupList = objectMapper.readValue(getResponse.readEntity(String.class),GroupList.class);
+    try {
+      GroupList groupList = objectMapper.readValue(getResponse.readEntity(String.class), GroupList.class);
       Iterator<Group> iterator = groupList.iterator();
       assertTrue(iterator.hasNext());
       Group group = iterator.next();
-      assertEquals("Gruppe7",group.getGroupName());
+      assertEquals("Gruppe7", group.getGroupName());
       assertEquals(6803, group.getGroupId());
 
 
-    } catch (JsonProcessingException e){
+    } catch (JsonProcessingException e) {
 
     }
   }
 
-  @Test
-  public void testGetUser(){
+  @Test public void testGetUser() {
     Response getResponse = target(WorkspaceService.WORKSPACE_SERVICE_PATH).path("userlist/get/olav")
-        .request(MediaType.APPLICATION_JSON + ";" + MediaType.CHARSET_PARAMETER+"=UTF8")
-        .get();
+        .request(MediaType.APPLICATION_JSON + ";" + MediaType.CHARSET_PARAMETER + "=UTF8").get();
     assertEquals(200, getResponse.getStatus());
 
-    try{
-      User user = objectMapper.readValue(getResponse.readEntity(String.class),User.class);
+    try {
+      User user = objectMapper.readValue(getResponse.readEntity(String.class), User.class);
       assertUser(user, "olav", "Olav", "Nordmann");
-    } catch(JsonProcessingException e){
+    } catch (JsonProcessingException e) {
 
     }
 
   }
 
-  @Test
-  public void getGroupIDtest(){
+  @Test public void getGroupIDtest() {
     Response getResponse = target(WorkspaceService.WORKSPACE_SERVICE_PATH).path("grouplist/get/6803")
-        .request(MediaType.APPLICATION_JSON + ";" + MediaType.CHARSET_PARAMETER+"=UTF8")
-        .get();
+        .request(MediaType.APPLICATION_JSON + ";" + MediaType.CHARSET_PARAMETER + "=UTF8").get();
     assertEquals(200, getResponse.getStatus());
 
-    try{
-      Group group = objectMapper.readValue(getResponse.readEntity(String.class),Group.class);
+    try {
+      Group group = objectMapper.readValue(getResponse.readEntity(String.class), Group.class);
       assertEquals(group.getGroupId(), 6803);
-    } catch(JsonProcessingException e){
+    } catch (JsonProcessingException e) {
 
     }
 
   }
 
-  /*@Test
-    public void newGroupTest(){
+  /*@Test public void newGroupTest() {
     Response getResponse = target(WorkspaceService.WORKSPACE_SERVICE_PATH).path("grouplist/new")
-        .request(MediaType.APPLICATION_JSON + ";" + MediaType.CHARSET_PARAMETER+"=UTF8")
-        .post()
+        .acc(MediaType.APPLICATION_JSON + ";" + MediaType.CHARSET_PARAMETER + "=UTF8").post(Entity.json("testerGroup"));
     assertEquals(200, getResponse.getStatus());
 
     Response getResponseGroup = target(WorkspaceService.WORKSPACE_SERVICE_PATH).path("grouplist")
-        .request(MediaType.APPLICATION_JSON + ";" + MediaType.CHARSET_PARAMETER+"=UTF8")
-        .get();
+        .request(MediaType.APPLICATION_JSON + ";" + MediaType.CHARSET_PARAMETER + "=UTF8").get();
     assertEquals(200, getResponseGroup.getStatus());
 
-    try{
-      Group group = objectMapper.readValue(getResponse.readEntity(String.class),Group.class);
-      GroupList groupList = objectMapper.readValue(getResponse.readEntity(String.class),GroupList.class);
+    try {
+      Group group = objectMapper.readValue(getResponse.readEntity(String.class), Group.class);
+      GroupList groupList = objectMapper.readValue(getResponse.readEntity(String.class), GroupList.class);
       assertTrue(groupList.contains(group));
-    } catch(JsonProcessingException e){
-
-    }*/
-
-  @Test
-  public void testgetJobshifts(){
-    Response getResponse = target(WorkspaceService.WORKSPACE_SERVICE_PATH).path("grouplist/get/6803/"+JOB_SHIFT_LIST_RESOURCE_PATH)
-        .request(MediaType.APPLICATION_JSON + ";" + MediaType.CHARSET_PARAMETER+"=UTF8")
-        .get();
-    assertEquals(200, getResponse.getStatus());
-
-    try{
-      JobShiftList jobShiftList = objectMapper.readValue(getResponse.readEntity(String.class),JobShiftList.class);
-      Iterator<JobShift> iterator = jobShiftList.iterator();
-      assertTrue(iterator.hasNext());
-      JobShift jobShift = iterator.next();
-      //assertEquals("7200", jobShift.getDuration().toSecondsPart());
-      assertEquals("2021-10-15T17:44:04.738",jobShift.getStartingTime().toString());
-      assertEquals("Dette er Olav sin skift.",jobShift.getInfo());
-
-    } catch(JsonProcessingException e){
+    } catch (JsonProcessingException e) {
 
     }
-
   }
+  */
+
 
   /*@Test
   public void testupdateJobshift(){
@@ -212,7 +177,46 @@ public class JobblettServiceTest extends JerseyTest{
 
 
 
+
+  @Test public void testGetJobshifts() {
+    Response getResponse = target(WorkspaceService.WORKSPACE_SERVICE_PATH).path("grouplist/get/6803/" + JOB_SHIFT_LIST_RESOURCE_PATH)
+            .request(MediaType.APPLICATION_JSON + ";" + MediaType.CHARSET_PARAMETER + "=UTF8").get();
+    assertEquals(200, getResponse.getStatus());
+
+    try {
+      JobShiftList jobShiftList = objectMapper.readValue(getResponse.readEntity(String.class), JobShiftList.class);
+      Iterator<JobShift> iterator = jobShiftList.iterator();
+      assertTrue(iterator.hasNext());
+      JobShift jobShift = iterator.next();
+      //assertEquals("7200", jobShift.getDuration().toSecondsPart());
+      assertEquals("2021-10-15T17:44:04.738", jobShift.getStartingTime().toString());
+      assertEquals("Dette er Olav sin skift.", jobShift.getInfo());
+
+    } catch (JsonProcessingException e) {
+
+    }
+
   }
+
+  @Test public void testGetJobshiftResource() {
+    Response getResponse = target(WorkspaceService.WORKSPACE_SERVICE_PATH)
+        .path("grouplist/get/6803/" + JOB_SHIFT_LIST_RESOURCE_PATH + "/get/0")
+        .request(MediaType.APPLICATION_JSON + ";" + MediaType.CHARSET_PARAMETER + "=UTF8").get();
+    assertEquals(200, getResponse.getStatus());
+
+    try {
+      JobShift jobShift = objectMapper.readValue(getResponse.readEntity(String.class), JobShift.class);
+      //assertEquals("7200", jobShift.getDuration().toSecondsPart());
+      assertEquals("2021-10-15T17:44:04.738", jobShift.getStartingTime().toString());
+      assertEquals("Dette er Olav sin skift.", jobShift.getInfo());
+
+    } catch (JsonProcessingException e) {
+
+    }
+
+  }
+
+}
   
 
     
