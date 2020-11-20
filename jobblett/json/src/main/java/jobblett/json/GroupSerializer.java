5f5a59059a73a38919d2a6b3df6ba8c297017e14
Kavusikan Sivasubramaniam <kavusiks@stud.ntnu.no>
2020-11-20 05:20:41 +0000

    customSerializers as intern in JSON,  removed unused imports ( #37)

--- jobblett/json/src/main/java/jobblett/json/GroupSerializer.java
+++ jobblett/json/src/main/java/jobblett/json/GroupSerializer.java
@@ -3,13 +3,12 @@ package jobblett.json;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
-import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import java.io.IOException;
 import java.util.ArrayList;
 import jobblett.core.Group;
 import jobblett.core.User;
 
-public class GroupSerializer extends JsonSerializer<Group> {
+class GroupSerializer extends JsonSerializer<Group> {
   @Override
   public void serialize(Group o, JsonGenerator jsonGenerator, SerializerProvider serializerProvider)
       throws IOException {
